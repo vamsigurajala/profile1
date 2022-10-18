052713c2944465c2ab32219abb47a4d8446680a4
vamsigurajala <gurajalakrishnavamsi456@gmail.com>
2022-10-18 22:55:04 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("random line...");
+        System.out.println("hii lebo");
     }
 }
